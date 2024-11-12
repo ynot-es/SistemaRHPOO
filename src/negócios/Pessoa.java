8e0b6463c7b2021ec779931c728e1ec50e8e4343
tony-esau <tonyesau10@gmail.com>
2024-11-12 13:23:24 -0300

    merda

--- src/negócios/Pessoa.java
+++ src/negócios/Pessoa.java
@@ -1,5 +1,21 @@
 package negócios;
 
-public class Pessoa {
-
+abstract class Pessoa {
+	private String nome;
+	private String cpf;
+	private String email;
+	private String endereco;
+	
+	public Pessoa(String nome, String cpf,String email) {
+		this.nome= nome;
+		this.cpf = cpf;
+		this.email = email;
+	}
+	
+	public void AtualizarDados(String nome, String email) {
+		this.nome =  nome;
+		this.email = email;
+	}
+	
 }
+
